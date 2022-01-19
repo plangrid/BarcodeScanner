2c2741a740f045f5c69e42a3929faf31008c8fce
Marco Vázquez <vazquezmarco3@gmail.com>
2022-01-19 11:43:04 -0600

    Separate protocols from main class

--- Sources/Controllers/BarcodeScannerViewController.swift
+++ Sources/Controllers/BarcodeScannerViewController.swift
@@ -1,41 +1,6 @@
 import UIKit
 import AVFoundation
 
-// MARK: - Delegates
-
-/// Delegate to handle the captured code.
-public protocol BarcodeScannerCodeDelegate: AnyObject {
-  func scanner(
-    _ controller: BarcodeScannerViewController,
-    didCaptureCode code: String,
-    type: String
-  )
-}
-
-/// Delegate to report errors.
-public protocol BarcodeScannerErrorDelegate: AnyObject {
-  func scanner(_ controller: BarcodeScannerViewController, didReceiveError error: BarcodeScannerError)
-}
-
-/// Delegate to dismiss barcode scanner when the close button has been pressed.
-public protocol BarcodeScannerDismissalDelegate: AnyObject {
-  func scannerDidDismiss(_ controller: BarcodeScannerViewController)
-}
-
-// MARK: - Error types
-
-public enum BarcodeScannerError: Error {
-  /// Error when something besides a MachineReadableCodeObject was detected.
-  /// (Check AVMetadataObject.ObjectType documentation)
-  case nonMachineReadableCodeDetected
-  /// Error describing an unexpected/general error
-  case unexpected(Error)
-  /// Error when a MachineReadableCodeObject was detected but its metadata is unsupported
-  case unsupported
-}
-
-// MARK: - Controller
-
 /**
  Barcode scanner controller with 4 sates:
  - Scanning mode
